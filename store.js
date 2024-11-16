9c0779759883065a70f47555eea9e5ab9a67f1b1
Deleronvi <byronochieng467@gmail.com>
2024-11-16 14:18:49 +0300

    complete adding items

--- store.js
+++ store.js
@@ -43,8 +43,41 @@ function addToCartClicked(event) {
     var button = event.target
     var shopItem = button.parentElement.parentElement
   var title = shopItem.getElementsByClassName('shop-item-title')[0].innerText
-  console.log(title)
+  var price = shopItem.getElementsByClassName('shop-item-price')[0].innerText
+  var imgSrc = shopItem.getElementsByClassName('shop-item-image')[0].src
+  console.log(title, price, imgSrc)
+  addItemToCart(title, price, imgSrc)
+updateCartTotal()
 }
+function addItemToCart(title, price, imgSrc){
+    var cartRow = document.createElement('div')
+    cartRow.classList.add('cart-row')
+var cartItems = document.getElementsByClassName('cart-items')[0]
+
+var cartItemNames = cartItems.getElementsByClassName('cart-item-title')
+for (var i = 0; i < cartItemNames.length; i++) {
+    if (cartItemNames[i].innerText == title) {
+        alert('This Item is already added to the cart')
+        return
+    }
+}
+
+    var cartRowContents = `
+    <div class="cart-item cart-column">
+        <img class="cart-item-image" src="${imgSrc}" width="100" height="100">
+        <span class="cart-item-title">${title}</span>
+    </div>
+    <span class="cart-price cart-column">${price}</span>
+    <div class="cart-quantity cart-column">
+        <input class="cart-quantity-input" type="number" value="1">
+        <button class="btn btn-danger" type="button">REMOVE</button>
+    </div>`
+    cartRow.innerHTML = cartRowContents
+    cartItems.append(cartRow)
+    cartRow.getElementsByClassName('btn-danger')[0].addEventListener('click', removeCartItem)
+    cartRow.getElementsByClassName('cart-quantity-input')[0].addEventListener('change', quantityChanged)
+}
+
 function updateCartTotal() {
     var cartItemContainer = document.getElementsByClassName('cart-items')[0]
     var cartRows = cartItemContainer.getElementsByClassName('cart-row')
